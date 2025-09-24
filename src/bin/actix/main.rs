439a1b985faedf646df84aa86acf9df4e06d08f3
Kondo Takeo <takeoverjp@gmail.com>
2025-09-25 00:29:50 +0900

    fix(actix): run all actors on only one Arbiter (OS thread)

--- src/bin/actix/main.rs
+++ src/bin/actix/main.rs
@@ -1,22 +1,24 @@
-use actix::prelude::*;
-
 mod heart_beat;
 mod platform;
 mod service;
 
-use heart_beat::HeartBeatActor;
-use platform::PlatformActor;
-use service::ServiceActor;
+use crate::heart_beat::HeartBeatActor;
+use crate::platform::PlatformActor;
+use crate::service::ServiceActor;
+use ::actix::prelude::*;
 
 #[actix::main]
 async fn main() {
     println!("main started");
-    // Start ServiceActor in new Arbiter
-    let arbiter = Arbiter::new();
-    arbiter.spawn_fn(|| {
+    // Start ServiceActor in current Arbiter
+    actix::spawn(async {
         ServiceActor.start();
         HeartBeatActor.start();
         PlatformActor.start();
-    });
-    arbiter.join().unwrap();
+
+        // run actors until Ctrl-C
+        ::tokio::signal::ctrl_c().await.unwrap();
+    })
+    .await
+    .unwrap();
 }
